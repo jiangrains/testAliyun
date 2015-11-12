cd532cfce1d5040779d847deecb8451de93b780c
jiangrains <jiangrains@126.com>
2015-11-12 17:58:24 +0800

    Update product.py

--- apps/models/default/product.py
+++ apps/models/default/product.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-from django.utils.translation import ugettext_lazy as _
-from shop.models_bases import BaseProduct
-from shop.models_bases.managers import (
+from thanksbody.models_bases import BaseProduct
+from thanksbody.models_bases.managers import (
     ProductManager,
     ProductStatisticsManager,
 )
@@ -13,6 +12,6 @@ class Product(BaseProduct):
 
     class Meta(object):
         abstract = False
-        app_label = 'shop'
-        verbose_name = _('Product')
-        verbose_name_plural = _('Products')
+        app_label = u"shop"
+        verbose_name = u"Product"
+        verbose_name_plural = u"Products"
