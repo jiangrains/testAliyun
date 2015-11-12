2f99f28dd1aee915c9b7dd6fdec18aec0e92e875
jiangrains <jiangrains@126.com>
2015-11-12 17:57:43 +0800

    Update orderitem.py

--- apps/models/default/orderitem.py
+++ apps/models/default/orderitem.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
-from django.utils.translation import ugettext_lazy as _
-from shop.models_bases import BaseOrderItem
+from thanksbody.models_bases import BaseOrderItem
 
 
 class OrderItem(BaseOrderItem):
 
     class Meta(object):
         abstract = False
-        app_label = 'shop'
-        verbose_name = _('Order item')
-        verbose_name_plural = _('Order items')
+        app_label = u"shop"
+        verbose_name = u"Order item"
+        verbose_name_plural = u"Order items"
